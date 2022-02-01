369947e8d742123f0c2744b6077efbbc8475702d
ActuallySam <srivastava.samarth.btech2018@sitpune.edu.in>
2022-02-01 20:01:21 +0530

    Fourth set of 3 Q's

---
+++ Milestone 6 - Walmart/12. Generate Random Point in a Circle.cpp
@@ -0,0 +1,35 @@
+/*
+Submitted by Amanullah Khan
+*/
+class Solution {
+public:
+    double r,x,y ;
+    Solution(double radius, double x_center, double y_center) {
+        
+        r = radius ;
+        x  = x_center ;
+        y =  y_center ;
+    }
+    
+    vector<double> randPoint() {
+        vector<double> v ;
+        double length ;
+        double theta ;
+        
+        double random  =  (double) rand() / RAND_MAX ;
+        theta = random * 2 * 3.14159 ;
+        
+         double random2 =  (double) rand() / RAND_MAX ;
+         length = sqrt(random2) * r ;
+        
+        double x_r = x + length*cos(theta) ;
+        double y_r = y + length * sin(theta) ;
+        
+        v.push_back(x_r) ;
+        v.push_back(y_r) ;
+        
+        return v ;
+        
+        
+    }
+};
